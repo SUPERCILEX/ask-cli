d3b42a43a682452daba1ac69fa9c28e84aca841a
Alex Saveau <saveau.alexandre@gmail.com>
2022-10-30 01:23:44 -0700

    Oh right, that won't work with Windows line endings

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 
 use std::{
     env, io,
-    io::{BufRead, Write},
+    io::{BorrowedBuf, Read, StdinLock, Write},
+    mem,
+    mem::MaybeUninit,
     process::ExitCode,
+    str::from_utf8,
 };
 
 fn main() -> ExitCode {
@@ -11,26 +14,59 @@ fn main() -> ExitCode {
     let mut stdout = io::stdout().lock();
     let mut stdin = io::stdin().lock();
 
-    let mut reply = String::new();
+    // max_len(yes, no, y, n) = 3 -> 3 + 2 bytes for new lines
+    let (mut buf, mut buf2) = ([MaybeUninit::uninit(); 5], [MaybeUninit::uninit(); 5]);
+    let (mut buf, mut buf2) = (
+        BorrowedBuf::from(buf.as_mut()),
+        BorrowedBuf::from(buf2.as_mut()),
+    );
 
-    loop {
+    'outer: loop {
         stdout.write_all(question.as_bytes()).unwrap();
         stdout.write_all(b"[Y/n] ").unwrap();
         stdout.flush().unwrap();
 
-        if stdin.read_line(&mut reply).unwrap() == 0 {
-            return ExitCode::FAILURE;
-        }
-        reply.pop().unwrap();
-        reply.make_ascii_lowercase();
-        match reply.as_str() {
+        let newline_index = if let Some(newline_index) = read_newline_index(&mut stdin, &mut buf) {
+            newline_index
+        } else {
+            loop {
+                buf.clear();
+                let prev_count = buf.len();
+
+                if let Some(newline_index) = read_newline_index(&mut stdin, &mut buf) {
+                    consume_newline(&mut buf, &mut buf2, newline_index);
+                    mem::swap(&mut buf, &mut buf2);
+                    continue 'outer;
+                }
+
+                if buf.len() == prev_count {
+                    // Reached EOF
+                    return ExitCode::FAILURE;
+                }
+            }
+        };
+
+        let reply = from_utf8(&buf.filled()[..newline_index]).unwrap();
+        // TODO https://github.com/rust-lang/rust/pull/103754
+        match reply.to_ascii_lowercase().as_str() {
             "" | "y" | "yes" => return ExitCode::SUCCESS,
             "n" | "no" => return ExitCode::FAILURE,
             _ => {
-                reply.clear();
+                consume_newline(&mut buf, &mut buf2, newline_index);
+                mem::swap(&mut buf, &mut buf2);
             }
         }
     }
+}
+
+fn consume_newline(buf: &mut BorrowedBuf, buf2: &mut BorrowedBuf, newline_index: usize) {
+    buf2.clear();
+    buf2.unfilled().append(&buf.filled()[newline_index + 1..]);
+}
+
+fn read_newline_index(stdin: &mut StdinLock, buf: &mut BorrowedBuf) -> Option<usize> {
+    stdin.read_buf(buf.unfilled()).unwrap();
+    buf.filled().iter().position(|b| *b == b'\n' || *b == b'\r')
 }
 
 fn question() -> String {
