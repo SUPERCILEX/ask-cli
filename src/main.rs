1e0c17a9784a4dfe6af3054d892f504a981839ad
Alex Saveau <saveau.alexandre@gmail.com>
2022-10-31 12:24:37 -0700

    Move to state machine architecture (hopefully that makes the control flow a little clearer)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,42 @@ use std::{
     process::ExitCode,
 };
 
+#[allow(clippy::too_many_lines)]
 fn main() -> ExitCode {
     // max_len(yes, no, y, n) = 3 -> 3 + 2 bytes for new lines
     const BUF_LEN: usize = 5;
+
+    enum State {
+        Start,
+        Ask {
+            /// Passthrough for [State::Read].
+            pending_crlf: bool,
+        },
+        /// Continuously reads from stdin until encountering a newline,
+        /// returning the index of its first byte.
+        ///
+        /// This state deals with a number of edge cases:
+        /// - If stdin reaches the EOF, exit the process *after* processing all
+        ///   remaining input.
+        /// - If a \r byte is seen on the border of the buffer, fail the input
+        ///   (because it will be at a higher index than the max length of all
+        ///   possible valid replies and therefore cannot be a valid reply) and
+        ///   consume a \n if it is the next byte. Note that this consumption
+        ///   cannot happen before printing the question again or we might get
+        ///   blocked on stdin if there are no more bytes available.
+        /// - If no newline was found within the buffer, fail the reply since it
+        ///   cannot possibly be valid.
+        Read {
+            /// The reply is known to be invalid, but we have not yet seen a
+            /// newline.
+            failed: bool,
+            /// A CRLF might be striding the buffer.
+            pending_crlf: bool,
+        },
+        HandleReply {
+            newline_index: usize,
+        },
+    }
 
     let (mut buf, mut buf2) = (
         [MaybeUninit::uninit(); BUF_LEN],
@@ -39,27 +72,30 @@ fn main() -> ExitCode {
         };
     }
 
+    let question = question();
     let mut stdin = io::stdin().lock();
-    let mut pending_crlf = false;
+    let mut stdout = io::stdout().lock();
 
-    /// Continuously reads from stdin until encountering a newline, returning
-    /// the index of its first byte.
-    ///
-    /// This function deals with a number of edge cases:
-    /// - If stdin reaches the EOF, exit the process *after* processing all
-    ///   remaining input.
-    /// - If a \r byte is seen on the border of the buffer, fail the input
-    ///   (because it will be at a higher index than the max length of all
-    ///   possible valid replies and therefore cannot be a valid reply) and
-    ///   consume a \n if it is the next byte. Note that this consumption cannot
-    ///   happen before printing the question again or we might get blocked on
-    ///   stdin if there are no more bytes available.
-    /// - If no newline was found within the buffer, fail the reply since it
-    ///   cannot possibly be valid.
-    macro_rules! read_line {
-        () => {{
-            let mut failed = false;
-            loop {
+    let mut state = State::Start;
+    loop {
+        state = match state {
+            State::Start => State::Ask {
+                pending_crlf: false,
+            },
+            State::Ask { pending_crlf } => {
+                stdout.write_all(question.as_bytes()).unwrap();
+                stdout.write_all(b"[Y/n] ").unwrap();
+                stdout.flush().unwrap();
+
+                State::Read {
+                    failed: false,
+                    pending_crlf,
+                }
+            }
+            State::Read {
+                failed,
+                pending_crlf,
+            } => {
                 debug_assert!(buf.len() < buf.capacity());
 
                 stdin.read_buf(buf.unfilled()).unwrap();
@@ -71,50 +107,46 @@ fn main() -> ExitCode {
                 if pending_crlf && buf.filled()[0] == b'\n' {
                     consume_bytes!(1);
                 }
-                pending_crlf = false;
 
-                if let Some(newline_index) =
-                    buf.filled().iter().position(|b| *b == b'\n' || *b == b'\r')
-                {
-                    break if newline_index == BUF_LEN - 1 {
-                        pending_crlf = buf.filled()[newline_index] == b'\r';
+                match buf.filled().iter().position(|b| *b == b'\n' || *b == b'\r') {
+                    Some(newline_index) if newline_index == BUF_LEN - 1 => {
+                        let pending_crlf = buf.filled()[newline_index] == b'\r';
                         buf.clear();
-                        None
-                    } else if failed {
+                        State::Ask { pending_crlf }
+                    }
+                    Some(newline_index) if failed => {
                         consume_newline!(newline_index);
-                        None
-                    } else {
-                        Some(newline_index)
-                    };
-                } else if buf.len() == buf.capacity() {
-                    failed = true;
-                    buf.clear();
+                        State::Ask {
+                            pending_crlf: false,
+                        }
+                    }
+                    Some(newline_index) => State::HandleReply { newline_index },
+                    None if buf.len() == buf.capacity() => {
+                        buf.clear();
+                        State::Read {
+                            failed: true,
+                            pending_crlf: false,
+                        }
+                    }
+                    None => State::Read {
+                        failed: false,
+                        pending_crlf: false,
+                    },
                 }
             }
-        }};
-    }
-
-    let question = question();
-    let mut stdout = io::stdout().lock();
-
-    loop {
-        stdout.write_all(question.as_bytes()).unwrap();
-        stdout.write_all(b"[Y/n] ").unwrap();
-        stdout.flush().unwrap();
-
-        let newline_index = if let Some(newline_index) = read_line!() {
-            newline_index
-        } else {
-            continue;
-        };
-
-        let reply = &buf.filled()[..newline_index];
-        // TODO https://github.com/rust-lang/rust/pull/103754
-        match reply.to_ascii_lowercase().as_slice() {
-            b"" | b"y" | b"yes" => return ExitCode::SUCCESS,
-            b"n" | b"no" => return ExitCode::FAILURE,
-            _ => {
-                consume_newline!(newline_index);
+            State::HandleReply { newline_index } => {
+                let reply = &buf.filled()[..newline_index];
+                // TODO https://github.com/rust-lang/rust/pull/103754
+                match reply.to_ascii_lowercase().as_slice() {
+                    b"" | b"y" | b"yes" => return ExitCode::SUCCESS,
+                    b"n" | b"no" => return ExitCode::FAILURE,
+                    _ => {
+                        consume_newline!(newline_index);
+                        State::Ask {
+                            pending_crlf: false,
+                        }
+                    }
+                }
             }
         }
     }
